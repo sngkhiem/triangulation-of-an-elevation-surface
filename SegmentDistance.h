4434c9e92021a408cb33c6608638775b5674b327
Tran Hoang Khiem <58633954+sngkhiem@users.noreply.github.com>
2024-05-11 09:05:27 +0700

    Header file to compute segment distance

---
+++ SegmentDistance.h
@@ -0,0 +1,16 @@
+#ifndef SEGMENTDISTANCE_H
+#define SEGMENTDISTANCE_H
+
+#include "qhull_tools.h"
+#include<Point.h>
+
+using namespace std;
+
+typedef Point<double> P;
+double segDist(P& s, P& e, P& p) {
+    if (s == e) return (p-s).dist();
+    auto d = (e-s).dist2(), t = min(d, max(.0, (p-s).dot(e-s)));
+    return ((p-s)*d-(e-s)*t).dist()/d;
+}
+
+#endif
