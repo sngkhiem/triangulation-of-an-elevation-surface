e667096a408633b1dffbf6621395f06f6067c55f
Tran Hoang Khiem <58633954+sngkhiem@users.noreply.github.com>
2024-05-11 09:04:07 +0700

    Header file for point represent

---
+++ Point.h
@@ -0,0 +1,29 @@
+#ifndef POINT_H
+#define POINT_H
+
+#include "qhull_tools.h"
+#include<math.h>
+#include<tuple>
+
+using namespace std;
+
+template <class T> int sgn(T x) { return (x > 0) - (x < 0); }
+template <class T>
+struct Point {
+    typedef Point P;
+    T x, y;
+    explicit Point(T x=0, T y=0) : x(x), y(y) {}
+    bool operator<(P p) const { return tie(x,y) < tie(p.x,p.y); }
+    bool operator==(P p) const { return tie(x,y)==tie(p.x,p.y); }
+    P operator+(P p) const { return P(x+p.x, y+p.y); }
+    P operator-(P p) const { return P(x-p.x, y-p.y); }
+    P operator*(T d) const { return P(x*d, y*d); }
+    P operator/(T d) const { return P(x/d, y/d); }
+    T dot(P p) const { return x*p.x + y*p.y; }
+    T cross(P p) const { return x*p.y - y*p.x; }
+    T cross(P a, P b) const { return (a-*this).cross(b-*this); }
+    T dist2() const { return x*x + y*y; }
+    double dist() const { return sqrt((double)dist2()); }
+};
+
+#endif
